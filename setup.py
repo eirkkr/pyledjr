b3f4c5f727defa025b78cd5cdd9f2cb134559e4c
kw0nta <eparkin985@gmail.com>
2019-08-28 22:58:43 +0800

    created setup

--- setup.py
+++ setup.py
@@ -1,13 +1,23 @@
-# Create data folder and path to folder
-# Ask user to enter credentials
+import setuptools
 
-from cryptography.fernet import Fernet
-key = Fernet.generate_key()
-f = Fernet(key)
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
-
-key = Fernet.gnerete('This is my secret key')
-
-token = f.encrypt(b'my deep dark secret')
-print(token)
-f.decrypt(token)
+setuptools.setup(
+    name="pyledjr",
+    version="0.0.1",
+    author="Eric Parkin",
+    author_email="eparkin985@gmail.com",
+    description=(
+        "A simple, open-source, python-based personal finace tool."
+    ),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kw0nta/pyledjr",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
